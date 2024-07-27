674a0c324177d3b5cd33879fdb991fb30154e80d
Laged <parkkila.matti@gmail.com>
2024-07-27 10:51:30 +0300

    pregpt

--- configuration.nix
+++ configuration.nix
@@ -1,10 +1,33 @@
 { modulesPath, pkgs, ... }:
+let
+  pub = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOQfsYJpXD/AFAxtC5aQ+mTyE367IyEipqNz5seu/d";
+in
 {
   imports = [ (modulesPath + "/installer/netboot/netboot-minimal.nix") ];
+  system.stateVersion = "24.05";
+  time.timeZone = "Europe/Helsinki";
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    keyMap = "fi";
+  };
 
   services.openssh.enable = true;
-  users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOrOQfsYJpXD/AFAxtC5aQ+mTyE367IyEipqNz5seu/d"
+  users.users.root.openssh.authorizedKeys.keys = [ pub ];
+
+  users.users.laged = {
+    isNormalUser = true;
+    home = "/home/laged";
+    initialPassword = "kek";
+    extraGroups = [
+      "wheel"
+      "networmanager"
+    ];
+    openssh.authorizedKeys.keys = [ pub ];
+  };
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
   ];
 
   environment.systemPackages = with pkgs; [
@@ -12,5 +35,11 @@
     pkgs.bat
     pkgs.bat-extras.batdiff
     vim
+    zsh
   ];
+
+  programs.zsh.enable = true;
+  users.defaultUserShell = pkgs.zsh;
+
+  services.networking.networkmanager.enable = true;
 }
